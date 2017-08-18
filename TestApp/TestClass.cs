280efb8d898bc640860b95275694be660a7f3a18
Todd Dobbs <btdobbs@gmail.com>
2017-08-17 22:14:01 -0400

    Initial Test Commit

---
+++ TestApp/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TestApp
+{
+    public class TestClass
+    {
+        public String MyProperty
+        {
+            get;
+            set;
+        }
+
+        public TestClass()
+        {
+        }
+    }
+}
